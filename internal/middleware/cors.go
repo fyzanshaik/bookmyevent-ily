1ece2564187c8e0400def0333e5ff5dc1a0a5ab3
fyzanshaik <fyzan.shaik@gmail.com>
2025-09-16 05:04:46 +0530

    cors issue with booking
    Signed-off-by: fyzanshaik <fyzan.shaik@gmail.com>

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -2,11 +2,37 @@ package middleware
 
 import (
 	"net/http"
+	"strings"
 )
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		origin := r.Header.Get("Origin")
+		
+		allowedOrigins := []string{
+			"http://localhost:3000",
+			"http://127.0.0.1:3000",
+			"http://165.232.181.30:3000", 
+		}
+		
+		allowedOrigin := ""
+		for _, allowed := range allowedOrigins {
+			if origin == allowed {
+				allowedOrigin = origin
+				break
+			}
+		}
+		
+		if allowedOrigin == "" && strings.Contains(origin, "localhost") {
+			allowedOrigin = origin
+		}
+		
+		if allowedOrigin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
+		} else {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+		}
+		
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-API-Key")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
@@ -23,7 +49,32 @@ func CORS(next http.Handler) http.Handler {
 
 func CORSHandler(handler http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		origin := r.Header.Get("Origin")
+		
+		allowedOrigins := []string{
+			"http://localhost:3000",
+			"http://127.0.0.1:3000",
+			"http://165.232.181.30:3000", 
+		}
+		
+		allowedOrigin := ""
+		for _, allowed := range allowedOrigins {
+			if origin == allowed {
+				allowedOrigin = origin
+				break
+			}
+		}
+		
+		if allowedOrigin == "" && strings.Contains(origin, "localhost") {
+			allowedOrigin = origin
+		}
+		
+		if allowedOrigin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
+		} else {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+		}
+		
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-API-Key")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
